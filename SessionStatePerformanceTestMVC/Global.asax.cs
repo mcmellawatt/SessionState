16df7982c1340b6dfd1cfb7ac8ca97e8005c75d5
Ryan McMahon <mcmellawat@gmail.com>
2018-01-17 10:26:37 -0800

    Setting up GemFire session state, adding .gitignore, removing unneccessary files

--- SessionStatePerformanceTestMVC/Global.asax.cs
+++ SessionStatePerformanceTestMVC/Global.asax.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Apache.Geode.Client;
+using Pivotal.GemFire.Session;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -14,6 +12,8 @@ namespace SessionStatePerformanceTestMVC
 
     public class MvcApplication : System.Web.HttpApplication
     {
+        private Cache Cache { get; set; }
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -22,6 +22,16 @@ namespace SessionStatePerformanceTestMVC
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Properties<string, string> properties = new Properties<string, string>();
+            properties.Load("c:\\dev\\gemfire-dotnetsession\\SampleWebApp\\geode.properties");
+            properties.Insert("cache-xml-file", "c:\\dev\\gemfire-dotnetsession\\SampleWebApp\\cache.xml");
+            properties.Insert("appdomain-enabled", "true");
+
+            var cacheFactory = CacheFactory.CreateCacheFactory(properties);
+
+            Cache = cacheFactory.Create();
+            Serializable.RegisterPdxSerializer(new GetItemExclusiveSerializer(new SessionStateStoreDataSerializer()));
         }
     }
 }
